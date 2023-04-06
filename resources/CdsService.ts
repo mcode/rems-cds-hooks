f1dff555a3682f9745900f43c7aefb6ef5eab675
Patrick LaRocque <plarocque@mitre.org>
2023-04-06 18:46:04 -0400

    Add the prefetch implementation and cds-hooks types from the rems-smart-on-fhir application..

---
+++ resources/CdsService.ts
@@ -0,0 +1,13 @@
+import { SupportedHooks } from "./HookTypes";
+
+export interface CdsService {
+  id: string;
+  title?: string;
+  description: string;
+  hook: SupportedHooks;
+  prefetch?: ServicePrefetch;
+}
+
+export interface ServicePrefetch {
+  [key: string]: string;
+}
